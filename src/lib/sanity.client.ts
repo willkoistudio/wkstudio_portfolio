1226010510e107ee276a83fb0a2b6999bd6cf7f3
William KOÏ <koi.william91@gmail.com>
2025-12-18 13:31:46 -0500

    Enable CDN usage in Sanity client configuration for improved performance.

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -6,5 +6,5 @@ export const sanity = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-01-01",
-  useCdn: false,
+  useCdn: true,
 });
